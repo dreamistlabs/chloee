bd90cca2793ac52fd31c5a03551620a305f32415
Johnny Hu <heyitsjhu@gmail.com>
2018-01-06 21:41:32 -0800

    Add uglify to script; write js to change font and input border color depending on hex/rgb

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,3 +1,13 @@
+function updateTextAndBorderColorBasedOn(color) {
+  if (color < 'aaaaaa') {
+    $('#Chloee').addClass('font--light');
+    $('input').addClass('border--light');
+  } else {
+    $('#Chloee').removeClass('font--light');
+    $('input').removeClass('border--light');
+  }
+}
+
 // focus handlers
 $('#hex-input, #rgb-input').focus(function() {
   $(this).parent('.input-group').addClass('active');
@@ -9,22 +19,26 @@ $('#hex-input, #rgb-input').focusout(function() {
 
 // capture input changes
 $('#hex-input').change(function() {
-  const HEXFormat = new RegExp(/^#*[0-9abcdef]{6}/, 'i');
+  const HEXFormat = new RegExp(/^#*[0-9a-f]{6}$/, 'i');
 
   if (this.value.match(HEXFormat)) {
     const rgbCode = new Chloee(this.value).convert();
     $('#rgb-input').val(rgbCode);
     $('#Chloee').css("background-color", `rgb(${rgbCode})`);
+
+    updateTextAndBorderColorBasedOn(this.value);
   }
 });
 
 $('#rgb-input').change(function() {
-  const RGBRange = '([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])';
-  const RGBFormat = new RegExp(`^(${RGBRange}[,\s]+){2}${RGBRange}{1,3}`);
+  const RANGE = '([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])';
+  const RGB_FORMAT = new RegExp(`^(${RANGE}[,\s]+){2}${RANGE}{1,3}`)
 
-  if (this.value.match(RGBFormat)) {
+  if (this.value.match(RGBFormat(RGB_FORMAT))) {
     const hexCode = new Chloee(this.value).convert();
     $('#hex-input').val(hexCode);
     $('#Chloee').css("background-color", `#${hexCode}`);
+
+    updateTextAndBorderColorBasedOn(hexCode);
   }
 });
