0bd94f9214d752761c3e882c80a83211b34fbf21
Johnny Hu <heyitsjhu@gmail.com>
2018-01-11 21:39:18 -0800

    Move from browserify to webpack for building production files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+/* Chloee - © 2017 Johnny Hu */
+
 import Chloee from './lib/Chloee';
 
 const chloee = color => new Chloee(color).convert();
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'chloee.min.js',
+    libraryTarget: 'umd',
+    library: 'chloee'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin()
+  ]
+}
